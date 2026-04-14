0adcf3dec834bfef4d39f5c134a27cc9ce40bf9e
JuanC <jcrodriguezba@esliceu.net>
2026-04-14 17:00:19 +0200

    video 07

---
+++ 002.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="JuanCarlos"
+#read serveix per llitgir informacío del teclat
+read -p "introueix un nom " NOM
+
+echo "El teu nom és ${NOM}"
